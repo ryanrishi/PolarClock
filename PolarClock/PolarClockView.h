b9ecbd75dd813956254c5610919fd6c7bb421997
Ryan Rishi <ryan@ryanrishi.com>
2025-07-22 09:46:33 -0400

    Initial Commit

---
+++ PolarClock/PolarClockView.h
@@ -0,0 +1,12 @@
+//
+//  PolarClockView.h
+//  PolarClock
+//
+//  Created by Ryan Rishi on 7/22/25.
+//
+
+#import <ScreenSaver/ScreenSaver.h>
+
+@interface PolarClockView : ScreenSaverView
+
+@end
